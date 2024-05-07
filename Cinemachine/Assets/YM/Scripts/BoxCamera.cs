a16b046df31e2ce754e99dc0dbd63e993e88c97d
Xion079 <rhdudals98@gmail.com>
2024-05-07 18:49:53 +0900

    드래곤 카메라 추가

--- Cinemachine/Assets/YM/Scripts/BoxCamera.cs
+++ Cinemachine/Assets/YM/Scripts/BoxCamera.cs
@@ -1,22 +1,37 @@
-using Cinemachine;
 using UnityEngine;
+using Cinemachine;
 
 public class BoxActivator : MonoBehaviour
 {
-    public CinemachineVirtualCamera virtualCamera;
+    public CinemachineVirtualCamera BoxCamera;
+    public CinemachineVirtualCamera BossCamera;
+
+    private void Start()
+    {
+        Invoke("BossCameraOn", 20f);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Box"))
         {
-            virtualCamera.m_Priority = 15;
-            Invoke("CameraOff", 3f);
-
+            BoxCamera.m_Priority = 15;
+            Invoke("BoxCameraOff", 3f);
         }
     }
 
-    private void CameraOff()
+    private void BoxCameraOff()
     {
-        virtualCamera.m_Priority = 5;
+        BoxCamera.m_Priority = 5;
+    }
+
+    private void BossCameraOn()
+    {
+        BossCamera.m_Priority = 15;
+        Invoke("BossCameraOff", 5f);
+    }
+    private void BossCameraOff()
+    {
+        BossCamera.m_Priority = 5;
     }
 }
