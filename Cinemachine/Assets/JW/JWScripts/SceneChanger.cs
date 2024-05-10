b21e0c1874c041e57d7c914ab70d073f597e58fe
jjaaww99 <157591426+jjaaww99@users.noreply.github.com>
2024-05-10 09:43:57 +0900

    0510 최종의최종의최종의최종

--- Cinemachine/Assets/JW/JWScripts/SceneChanger.cs
+++ Cinemachine/Assets/JW/JWScripts/SceneChanger.cs
@@ -21,7 +21,7 @@ public class SceneChanger : MonoBehaviour
             int currentIndex = SceneManager.GetActiveScene().buildIndex;
             int nextIndex = currentIndex + 1;
 
-            if(nextIndex < 2)
+            if(nextIndex < 3)
             {
                SceneManager.LoadScene(nextIndex);
             }
